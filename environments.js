19583204cac9fa5b721882d034b33c7c466ccfdb
Thomas Fries <thomas.fries@bosch-si.com>
2021-07-31 18:33:34 +0200

    separate settings json

---
+++ environments.js
@@ -0,0 +1,17 @@
+export let settings = {
+    local_ditto: {
+        api_uri: 'http://localhost:8080',
+        solutionId: null,
+        bearer:  null,
+        usernamePassword: 'ditto:ditto',
+        useBasicAuth: true
+    },
+    cloud_things_aws: {
+        api_uri: 'https://things.eu-1.bosch-iot-suite.com',
+        solutionId: null,
+        bearer:  null,
+        usernamePassword: null,
+        useBasicAuth: false
+    }
+};
+
